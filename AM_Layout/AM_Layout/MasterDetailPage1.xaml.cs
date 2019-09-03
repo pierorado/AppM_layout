1ce0ab9d1b3a702c4abdbfaecaa585a6ffb66181
delpiero ramos ramos <delpiee@hotmail.com>
2019-09-02 19:19:12 -0500

    commit 2

---
+++ AM_Layout/AM_Layout/MasterDetailPage1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AM_Layout
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterDetailPage1 : ContentPage
+    {
+        public MasterDetailPage1()
+        {
+            InitializeComponent();
+        }
+    }
+}
